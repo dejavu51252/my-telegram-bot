4933a6a9194df7c808c2c14cde4e810f8a5a1faa
dejavu51252 <nurislombekov@gmail.com>
2026-09-08 17:09:33 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-TOKEN = "8314078721:AAG2FiCKSx2cQdFE4ml7cRuf2DTHwPHuXHs"  # Твой токен
+TOKEN = "8314078721:AAHE3KFuFWAo8mis4tP1fUuPozaBHFGqXKQ"  # Твой токен
 
 def get_main_keyboard():
     keyboard = [
